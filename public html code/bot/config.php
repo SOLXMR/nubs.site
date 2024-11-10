350881b29c2e77c5e1f6079c28ee6093afa5b3a5
MoneroSolana <new71mail111GG9@proton.me>
2024-11-10 00:17:34 -0800

    Remove all files from repository

--- public html code/bot/config.php
+++
@@ -1,4 +0,0 @@
-<?php
-define("BOT_TOKEN", "7618736848:AAE0Via8SDzdSMyQRtHyKUHY3Ivt3uZKAOg");
-define("CPANEL_USER", "nubsmrbf");
-?>
--- public html code/sodium_test.php
+++
@@ -1,7 +0,0 @@
-<?php
-if (function_exists('sodium_crypto_sign_verify_detached')) {
-    echo "Sodium library is available";
-} else {
-    echo "Sodium library is not available";
-}
-?>
